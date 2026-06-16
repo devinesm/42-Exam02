767792244933811e63bbc395cc992f4b4ee90c4f
devinesm <inesmonteirosara@gmail.com>
2026-06-16 14:17:19 +0100

    add finish level 2

--- 2/str_capitalizer.c
+++ 2/str_capitalizer.c
@@ -6,8 +6,10 @@ Write a program that takes one or several strings and, for each argument,
 capitalizes the first character of each word (if it's a letter), puts the
 rest in lowercase, and displays the result on the standard output, followed
 by a \n.
+
 A "word" is defined as a part of a string delimited either by spaces/tabs, or
 by the start/end of the string.
+
 If there are no arguments, the program must display \n.
 
 Example:
@@ -21,3 +23,39 @@ __second Test A Little Bit Moar Complex$
 But... This Is Not That Complex$
 
 */
+#include <unistd.h>
+
+void str_capitalizer(char *s)
+{
+	int i = 0;
+	while (s[i])
+	{
+		char c = s[i];
+		if (c >= 'A' && c <= 'Z')
+			c += 32;
+
+		int is_first = ((i == 0) || (s[i - 1] == ' ' || s[i - 1] == '\t'));
+		if (is_first == 1 && (c >= 'a' && c <= 'z'))
+			c -= 32;
+
+		write(1, &c, 1);
+		i++;
+	}
+}
+
+int main(int ac, char **av)
+{
+	if (ac >= 2)
+	{
+		int i = 1;
+		while (i < ac)
+		{
+			str_capitalizer(av[i]);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	else
+		write(1, "\n", 1);
+	return (0);
+}
--- 2/tab_mult.c
+++ 2/tab_mult.c
@@ -18,3 +18,45 @@ $>./tab_mult | cat -e
 $
 
 */
+
+#include <unistd.h>
+void ft_putnbr(int number)
+{
+	if (number >= 10) ft_putnbr(number / 10);
+	char c = number % 10 + '0';
+	write(1, &c, 1);
+}
+
+int ft_atoi(char *s)
+{
+	int i = 0;
+	int result = 0;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (result);
+}
+
+int main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int i = 1;
+		int limit = ft_atoi(av[1]);
+		while (i <= 9)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(limit);
+			write(1, " = ", 3);
+			ft_putnbr(i * limit);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	else
+		write(1, "\n", 1);
+	return (0);
+}
